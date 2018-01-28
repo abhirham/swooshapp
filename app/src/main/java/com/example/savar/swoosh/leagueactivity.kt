f0d9c2dfa1810d777378390d58c783e2bbeff62a
Abhirham <savarav.a@gmail.com>
2018-01-28 10:21:36 -0500

    added a new page

---
+++ app/src/main/java/com/example/savar/swoosh/leagueactivity.kt
@@ -0,0 +1,12 @@
+package com.example.savar.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class leagueactivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leagueactivity)
+    }
+}
